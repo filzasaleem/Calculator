df9bfb1fbc14975edc75e3589a21de2c8e2ca6fd
Filza Saleem <filza.saleem05@gmail.com>
2013-03-08 09:25:27 +0100

    added division and multiplication

--- ConsoleApplication3/Program.cs
+++ ConsoleApplication3/Program.cs
@@ -10,22 +10,19 @@ namespace ConsoleApplication3
     {
         static void Main(string[] args)
         {
-            //Counter c = new Counter();
-            //c.Add(3);
-            //c.Add(17);
-            //Console.WriteLine("total: {0}, average: {1}", c.Value, c.Mean());
+            Console.WriteLine("enter three values for Addition");
+            Expression.Abstract expression0 = new Expression.Addition() { Left = new Expression.Number(float.Parse(Console.ReadLine())), Right = new Expression.Number(float.Parse(Console.ReadLine())) };
+            Expression.Abstract expression1 = new Expression.Addition() { Left = expression0, Right = new Expression.Number(float.Parse(Console.ReadLine())) };
+            Console.WriteLine("enter 2 values for Subtraction");
+            Expression.Abstract expression2 = new Expression.Subtraction() { Left = new Expression.Number(float.Parse(Console.ReadLine())), Right = new Expression.Number(float.Parse(Console.ReadLine())) };
+            Console.WriteLine("enter 2 values for Multiplication");
+            Expression.Abstract expression3 = new Expression.Multiplication() { Left = new Expression.Number(float.Parse(Console.ReadLine())), Right = new Expression.Number(float.Parse(Console.ReadLine())) };
+            Console.WriteLine("enter 2 values for Division");
+            Expression.Abstract expression4 = new Expression.Division() { Left = new Expression.Number(float.Parse(Console.ReadLine())), Right = new Expression.Number(float.Parse(Console.ReadLine())) };
 
-            Expression.Abstract expression0 = new Expression.Addition() { Left = new Expression.Number(2.3f), Right = new Expression.Number(1.2f) };
-            Expression.Abstract expression1 = new Expression.Addition() { Left = expression0, Right = new Expression.Number(3f) };
-            //Console.WriteLine(expression0);
-            //Console.WriteLine(expression1 + " = " + expression1.Evaluate());
-
-
-            Expression.Abstract expression2 = new Expression.Subtraction() { Left = new Expression.Number(2.3f), Right = new Expression.Number(1.2f) };
-//            Expression.Abstract expression3 = new Expression.Subtraction() { Left = expression2, Right = new Expression.Number(0.1f) };
-            Expression.Abstract expression3 = expression1 - 0.1f - expression1;
-           // Console.WriteLine(expression2);
-            Console.WriteLine(expression3 + " = " + expression3.Evaluate());
+            Expression.Abstract expression5 = expression1 - expression3 - 0.1f - expression4  - expression1;
+          
+            Console.WriteLine(expression5 + " = " + expression5.Evaluate());
             Console.ReadKey();
         }
     }
