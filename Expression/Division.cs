d3cfae6a4d0a55c8829bfe9ac77709cab082baec
Filza Saleem <filza.saleem05@gmail.com>
2013-03-12 10:12:16 +0100

    class Division and MUltiplication

---
+++ Expression/Division.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expression
+{
+   public class Division:
+       BinaryOperator 
+    {
+        protected override int Precedence { get { return 2; } }
+        protected override string Symbol { get { return "/"; } }
+        public override float Evaluate()
+        {
+            return this.Left.Evaluate() / this.Right.Evaluate();
+        }
+    }
+}
---
+++ Expression/Multiplication.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expression
+{
+   public class Multiplication:
+         BinaryOperator
+    {
+        protected override int Precedence{get {return 1;}}
+        protected override string Symbol { get { return "*"; } }
+        public override float Evaluate()
+        {
+            return this.Left.Evaluate() * this.Right.Evaluate();
+        }
+
+    }
+}
