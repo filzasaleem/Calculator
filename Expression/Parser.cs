d5f38c12570a33992feb4f3b2aeec76583720cf8
filzasaleem <filza.saleem05@gmail.com>
2013-07-09 15:31:36 +0200

    modified Parser.cs

--- Expression/Parser.cs
+++ Expression/Parser.cs
@@ -7,59 +7,54 @@ namespace Expression
 {
 	public class Parser
 	{
-	    public Parser()
-	    {
+		public Parser()
+		{
 		}
 		Collection.IStack<Abstract> stack = new Collection.Stack<Abstract>();
-		Token.Abstract token = null ;
 		public Abstract Parse(string expression)
 		{
-			return this.Parse(null,Token.Abstract.Tokenize(expression));
+			return this.Parse(null, Token.Abstract.Tokenize(expression));
 		}
-		Abstract Parse(Token.Abstract @operator, Collection.IQueue<Token.Abstract> arguments)
+		Abstract Parse(Token.Abstract operator1, Collection.IQueue<Token.Abstract> arguments)
 		{
 			Abstract left;
 			Abstract right;
-			if(@operator == null)
-				@operator = PushOnStack(arguments);
-			if (this.token == null)
-				this.token = PushOnStack(arguments);
+			Token.Abstract operator2 = null; 
+			if (operator1.IsNull())
+				operator1 = this.PushOnStack(arguments);
+			if (operator2.IsNull())
+				operator2 = this.PushOnStack(arguments);
 			if (arguments.Count > 0)
 			{
 				if (arguments.Peek() is Token.Number)
-				{
-					Abstract number = new Expression.Number((arguments.Dequeue() as Token.Number).Value);
-					this.stack.Push(number);
-				}
+					this.stack.Push(new Expression.Number((arguments.Dequeue() as Token.Number).Value));
 				else if (arguments.Peek() is Token.Variable)
-				{
-					Abstract name = new Expression.Variable((arguments.Dequeue() as Token.Variable).Name);
-					this.stack.Push(name);
-				}
+					this.stack.Push(new Expression.Variable((arguments.Dequeue() as Token.Variable).Name));
 			}
 			right = this.stack.Pop();
 			left = this.stack.Pop();
-			if (@operator != null && this.token != null && @operator.Precedence < this.token.Precedence)
+			if (operator1.NotNull())
 			{
-				this.stack.Push((this.token as Token.BinaryOperator).Create(left, right));
-				this.token = null;
-				Parse(@operator, arguments);
+				if (operator2.NotNull())
+				{
+					if (operator1.Precedence < operator2.Precedence)
+						this.stack.Push((operator2 as Token.BinaryOperator).Create(left, right));
+					else if (operator1.Precedence > operator2.Precedence || operator1.Precedence == operator2.Precedence)
+					{
+						Abstract temp = this.stack.Pop();
+				        this.stack.Push((operator1 as Token.BinaryOperator).Create(temp, left));
+				        this.stack.Push(right);
+						operator1 = operator2;
+					}
+					this.Parse(operator1, arguments);
+				}
+				else
+				{
+					this.stack.Push((operator1 as Token.BinaryOperator).Create(left, right));
+				    operator1 = null;
+				}
 			}
-			else if ((@operator != null && this.token != null) && (@operator.Precedence > this.token.Precedence || @operator.Precedence == this.token.Precedence))
-			{
-				Abstract temp = this.stack.Pop();
-				this.stack.Push((@operator as Token.BinaryOperator).Create(temp, left));
-				this.stack.Push(right);
-				@operator = this.token;
-				this.token = null;
-				Parse(@operator, arguments);
-			}
-			else if (@operator != null && this.token == null)
-			{
-				this.stack.Push((@operator as Token.BinaryOperator).Create(left, right));
-				@operator = null;
-			}
-		   return this.stack.Peek();
+			return this.stack.Peek();
 		}
 		public Token.Abstract PushOnStack(Collection.IQueue<Token.Abstract> arguments)
 		{
@@ -68,14 +63,12 @@ namespace Expression
 			{
 				if (arguments.Peek() is Token.Number)
 				{
-					Abstract number = new Expression.Number((arguments.Dequeue() as Token.Number).Value);
-					this.stack.Push(number);
+					this.stack.Push(new Expression.Number((arguments.Dequeue() as Token.Number).Value));
 					result = PushOnStack(arguments);
 				}
 				else if (arguments.Peek() is Token.Variable)
 				{
-					Abstract name = new Expression.Variable((arguments.Dequeue() as Token.Variable).Name);
-					this.stack.Push(name);
+					this.stack.Push(new Expression.Variable((arguments.Dequeue() as Token.Variable).Name));
 					result = PushOnStack(arguments);
 				}
 				else if (arguments.Peek() is Token.BinaryOperator)
--- Expression/Token/Abstract.cs
+++ Expression/Token/Abstract.cs
@@ -10,51 +10,48 @@ namespace Expression.Token
 		{
 			System.Text.StringBuilder digit = new System.Text.StringBuilder();
 			Collection.IQueue<Token.Abstract> arguments = new Collection.Queue<Abstract>();
-		    foreach(char c in data)
+			foreach (char c in data)
 			{
 				if (char.IsDigit(c))
-				{
 					digit.Append(c);
-					continue;
-				}
 				else if (c == '.')
-				{
 					digit.Append(c);
-					continue;
-				}
-			    else if(digit.Length > 0)
-				{
-					arguments.Enqueue(new Token.Number(float.Parse(digit.ToString())));
-					digit = new System.Text.StringBuilder();
-				}
-				if (char.IsLetter(c))
-					arguments.Enqueue(new Token.Variable(c.ToString()));
 				else
-					switch (c)
+				{
+					if (digit.Length > 0)
 					{
-						case '-':
-							arguments.Enqueue(new Token.Subtraction());
-							break;
-						case '+':
-							arguments.Enqueue(new Token.Addition());
-							break;
-						case '*':
-							arguments.Enqueue(new Token.Multiplication());
-							break;
-						case '/':
-							arguments.Enqueue(new Token.Division());
-							break;
-						case '^':
-							arguments.Enqueue(new Token.Power());
-							break;
+						arguments.Enqueue(new Token.Number(float.Parse(digit.ToString())));
+						digit = new System.Text.StringBuilder();
 					}
+					if (char.IsLetter(c))
+						arguments.Enqueue(new Token.Variable(c.ToString()));
+					else
+						switch (c)
+						{
+							case '-':
+								arguments.Enqueue(new Token.Subtraction());
+								break;
+							case '+':
+								arguments.Enqueue(new Token.Addition());
+								break;
+							case '*':
+								arguments.Enqueue(new Token.Multiplication());
+								break;
+							case '/':
+								arguments.Enqueue(new Token.Division());
+								break;
+							case '^':
+								arguments.Enqueue(new Token.Power());
+								break;
+						}
+				}
 			}
 			if (digit.Length > 0)
 			{
 				arguments.Enqueue(new Token.Number(float.Parse(digit.ToString())));
 				digit = new System.Text.StringBuilder();
 			}
-		    return arguments;
+			return arguments;
 		}
 	}
 }
