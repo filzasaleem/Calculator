e3a8a487710636513bd03043d3b0c13073b7c683
filzasaleem <filza.saleem05@gmail.com>
2013-07-16 16:57:40 +0200

    added graphical user interface.

---
+++ Calculator/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = ((Expression.Abstract)this.textBox1.Text).Evaluate().ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = ((Expression.Abstract)this.textBox1.Text).Simplify().ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Expression.Abstract v = (Expression.Abstract)this.textBox1.Text.ToString();
+            this.textBox1.Text = new Expression.Sine(v).Evaluate().ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Expression.Abstract z = (Expression.Abstract)this.textBox1.Text.ToString();
+            this.textBox1.Text = z.Derive("x").Simplify().ToString();
+        }
+
+    }
+}
--- Expression/Parser.cs
+++ Expression/Parser.cs
@@ -17,8 +17,8 @@ namespace Expression
 		}
 		Abstract Parse(Token.Abstract operator1, Collection.IQueue<Token.Abstract> arguments)
 		{
-			Abstract left;
-			Abstract right;
+			Abstract left = null;
+			Abstract right = null;
 			Token.Abstract operator2 = null;
 			if (operator1.IsNull())
 				operator1 = this.PushOnStack(operator1, arguments);
@@ -26,34 +26,38 @@ namespace Expression
 				operator2 = this.PushOnStack(operator2, arguments);
             if (!(operator1 is Token.RightParanthesis) && !(operator2 is Token.RightParanthesis))
 			    operator2 = this.PushOnStack(operator2, arguments);
-			right = this.stack.Pop();
-			left = this.stack.Pop();
-			if (operator1.NotNull())
-			{
-				if (operator2.NotNull())
-				{
-					if (operator2 is Token.RightParanthesis)
-						this.stack.Push((operator1 as Token.BinaryOperator).Create(left, right));
-					else
-					{
+            if (!(this.stack.Empty))
+			   right = this.stack.Pop();
+            if (!(this.stack.Empty))
+			   left = this.stack.Pop();
+            if (operator1.NotNull() && right.NotNull() && left.NotNull())
+            {
+                if (operator2.NotNull())
+                {
+                    if (operator2 is Token.RightParanthesis)
+                        this.stack.Push((operator1 as Token.BinaryOperator).Create(left, right));
+                    else
+                    {
                         if (operator1.Precedence < operator2.Precedence && !(operator1 is Token.RightParanthesis))
-							this.stack.Push((operator2 as Token.BinaryOperator).Create(left, right));
-						else if (operator1.Precedence > operator2.Precedence || operator1.Precedence == operator2.Precedence)
-						{
-							Abstract temp = this.stack.Pop();
-							this.stack.Push((operator1 as Token.BinaryOperator).Create(temp, left));
-							this.stack.Push(right);
-							operator1 = operator2;
-						}
-						this.Parse(operator1, arguments);
-					}
-				}
-				else
-				{
-					this.stack.Push((operator1 as Token.BinaryOperator).Create(left, right));
-				    operator1 = null;
-				}
-			}
+                            this.stack.Push((operator2 as Token.BinaryOperator).Create(left, right));
+                        else if (operator1.Precedence > operator2.Precedence || operator1.Precedence == operator2.Precedence)
+                        {
+                            Abstract temp = this.stack.Pop();
+                            this.stack.Push((operator1 as Token.BinaryOperator).Create(temp, left));
+                            this.stack.Push(right);
+                            operator1 = operator2;
+                        }
+                        this.Parse(operator1, arguments);
+                    }
+                }
+                else
+                {
+                    this.stack.Push((operator1 as Token.BinaryOperator).Create(left, right));
+                    operator1 = null;
+                }
+            }
+            else
+                this.stack.Push(right);
 			return this.stack.Peek();
 		}
 		public Token.Abstract PushOnStack(Token.Abstract @operator, Collection.IQueue<Token.Abstract> arguments)
